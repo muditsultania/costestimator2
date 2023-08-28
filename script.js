cc849e76e740ab021b0b3e71d895cc4159b4dfa8
muditsultania <92985947+muditsultania@users.noreply.github.com>
2023-08-28 14:17:08 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,185 +1,281 @@
-const roomTypes = [
-  "Living Room",
-  "Bedroom",
-  "Kitchen",
-  "Bathroom",
-  "Dining Room",
-  "Study Room",
-  "Other"
-];
+// Constants and variables for factors and prices
+const basePrices = {
+  warmshell: { Mumbai: 3500, Ahmedabad: 2000, Hyderabad: 2500 },
+  bareshell: { Mumbai: 4250, Ahmedabad: 2750, Hyderabad: 3250 },
+  renovation: { Mumbai: 4000, Ahmedabad: 2500, Hyderabad: 3000 },
+  architecture: { Mumbai: 6000, Ahmedabad: 5000, Hyderabad: 6000 },
+};
 
-const workCategories = [
-  "Painting",
-  "Furniture",
-  "Lighting",
-  "Flooring",
-  "Plumbing",
-  "Electrical",
-  "Other"
-];
+const propertyFactors = {
+  Bungalow: 1.05,
+  Apartment: 1,
+  Farmhouse: 0.95,
+  Villa: 1.07,
+};
 
-const plans = [
-  "Premium",
-  "Premium+",
-  "Luxury"
-];
+const stylingFactors = {
+  Low: 1,
+  Medium: 1.1,
+  High: 1.2,
+  "Very High": 1.3,
+};
 
-const roomsContainer = document.getElementById("rooms-container");
-const calculateBtn = document.getElementById("calculate-btn");
-const resultContainer = document.getElementById("result");
+const ceilingHeightFactors = {
+  "10ft": 1,
+  "11ft": 1.05,
+  "12ft": 1.1,
+  "13ft": 1.15,
+  "14ft": 1.2,
+  "14ft+": 1.5,
+};
 
-calculateBtn.addEventListener("click", calculateEstimate);
+const planFactors = {
+  Premium: 1,
+  "Premium Plus": 1.2,
+  Luxury: 1.5,
+  "Ultra Luxury": 2,
+};
 
-const bhkSelect = document.getElementById("bhk");
-bhkSelect.addEventListener("change", handleBHKChange);
+const addOnPrices = {
+  "Automation": { Mumbai: 350, Ahmedabad: 200, Hyderabad: 250 },
+  "Civil Changes": { Mumbai: 150, Ahmedabad: 50, Hyderabad: 100 },
+  "Fire Fighting": { Mumbai: 25, Ahmedabad: 25, Hyderabad: 25 },
+  "Flooring": 	{Mumbai: 350,	Ahmedabad: 200, Hyderabad:	250},
+  "Hvac": 	{Mumbai: 450, Ahmedabad: 400,	Hyderabad: 400},
+  "House Keeping": 	{Mumbai: 20, Ahmedabad: 12.5, Hyderabad:	15},
+  "Pest Control":	{Mumbai: 10, Ahmedabad:	10, Hyderabad:	10},
+  "Surveillance":	{Mumbai: 50, Ahmedabad:	50, Hyderabad:	50}
 
-function handleBHKChange() {
-  const selectedBHK = bhkSelect.value;
-  const numRooms = parseInt(selectedBHK) || 0;
+  // ...other add-ons
+};
 
-  roomsContainer.innerHTML = ""; // Clear previous rooms
-
-  for (let i = 0; i < numRooms; i++) {
-    const roomDiv = createRoomFields(i);
-    roomsContainer.appendChild(roomDiv);
-  }
-}
-
-// ...
-function createRoomFields(roomIndex) {
-  const roomDiv = document.createElement("div");
-  roomDiv.classList.add("room");
-
-  const roomTitle = document.createElement("h3");
-  roomTitle.textContent = `Room ${roomIndex + 1}`;
-  roomDiv.appendChild(roomTitle);
-
-  const roomTypeLabel = document.createElement("label");
-  roomTypeLabel.textContent = "Select Room Type:";
-
-  const roomTypeSelect = document.createElement("select");
-  for (const type of roomTypes) {
-    const option = document.createElement("option");
-    option.value = type;
-    option.text = type;
-    roomTypeSelect.appendChild(option);
-  }
-
-  roomTypeLabel.appendChild(roomTypeSelect);
-  roomDiv.appendChild(roomTypeLabel);
-
-  // Create "Add On" checkbox
-  const addOnCheckboxLabel = document.createElement("label");
-  const addOnCheckbox = document.createElement("input");
-  addOnCheckbox.type = "checkbox";
-  addOnCheckbox.name = `add-on-${roomIndex}`;
-  addOnCheckbox.value = "addOn";
-  addOnCheckboxLabel.textContent = "Add On";
-  addOnCheckboxLabel.appendChild(addOnCheckbox);
-  roomDiv.appendChild(addOnCheckboxLabel);
-
-  // Container for dynamic "Add On" elements
-  const addOnContainer = document.createElement("div");
-  roomDiv.appendChild(addOnContainer);
-
-  // Handle "Add On" checkbox change event
-  addOnCheckbox.addEventListener("change", function () {
-    if (addOnCheckbox.checked) {
-      createAddOnElements(addOnContainer, roomIndex);
-    } else {
-      addOnContainer.innerHTML = ""; // Clear "Add On" elements when checkbox is unchecked
-    }
-  });
-
-  return roomDiv;
-}
-function createAddOnElements(container, roomIndex, level = 1) {
-  const addOnDropdown = document.createElement("select");
-  addOnDropdown.name = `add-ons-${roomIndex}-${level}`;
-  
-  const defaultOption = document.createElement("option");
-  defaultOption.value = "";
-  defaultOption.text = "Select a category";
-  defaultOption.disabled = true;
-  defaultOption.selected = true;
-  addOnDropdown.appendChild(defaultOption);
-
-  for (const category of workCategories) {
-    const option = document.createElement("option");
-    option.value = category;
-    option.text = category;
-    addOnDropdown.appendChild(option);
-  }
-
-  const addOnCheckbox = document.createElement("input");
-  addOnCheckbox.type = "checkbox";
-  addOnCheckbox.name = `add-on-${roomIndex}-${level}`;
-  addOnCheckbox.value = `addOn-${level}`;
-
-  // Create "Add On" checkbox label
-  const addOnCheckboxLabel = document.createElement("label");
-  addOnCheckboxLabel.textContent = `Add On`;
-  addOnCheckboxLabel.appendChild(addOnCheckbox);
-
-  // Append the dropdown and the "Add On" checkbox label
-  container.appendChild(addOnDropdown);
-  container.appendChild(addOnCheckboxLabel);
-
-  // Handle "Add On" checkbox change event for the current level
-  addOnCheckbox.addEventListener("change", function () {
-    if (addOnCheckbox.checked) {
-      createAddOnElements(container, roomIndex, level + 1);
-    } else {
-      // Clear this and higher levels when checkbox is unchecked
-      let nextLevelContainer = container.querySelector(`[name="add-ons-${roomIndex}-${level + 1}"]`);
-      while (nextLevelContainer) {
-        nextLevelContainer.remove();
-        nextLevelContainer = container.querySelector(`[name="add-ons-${roomIndex}-${level + 1}"]`);
-      }
-    }
-  });
-}
-
-
-  
-
-function addCategoryToRoom(categorySelect) {
-  const selectedCategory = categorySelect.value;
-  if (selectedCategory) {
-    categorySelect.options[categorySelect.selectedIndex].disabled = true;
-  }
-}
 
 function calculateEstimate() {
-  const totalEstimate = 2000; // Placeholder value, you can calculate this based on your requirements
+  const projectType =  document.querySelector('.button-option-project.button-selected').getAttribute('data-value');
+  const city = document.querySelector('.button-option-city.button-selected').getAttribute('data-value');
+  const propertyType = document.querySelector('.button-option-property.button-selected').getAttribute('data-value');
+  const styling = document.querySelector('.button-option-styling.button-selected').getAttribute('data-value');
+  const ceilingHeight =document.querySelector('.button-option-ceiling.button-selected').getAttribute('data-value');
+  const plan =document.querySelector('.button-option-plan.button-selected').getAttribute('data-value');
+  const carpetArea =  parseFloat(document.getElementById('carpet-area').value);
+  const addOns = document.querySelectorAll('input[name="add-on"]:checked');
+  const addOnValues = Array.from(addOns).map((addOn) => addOn.value);
 
-  resultContainer.innerHTML = `<p>Your estimated price is: $${totalEstimate}</p>`;
+  const basePrice = basePrices[projectType][city] * carpetArea;
+  const propertyFactor = propertyFactors[propertyType];
+  const stylingFactor = stylingFactors[styling];
+  const ceilingHeightFactor = ceilingHeightFactors[ceilingHeight];
+  const planFactor = planFactors[plan];
+  const combinedFactor = propertyFactor * stylingFactor * ceilingHeightFactor * planFactor;
+
+  let estimatedPrice = basePrice * combinedFactor;
+
+  // Calculate add-ons cost
+  addOnValues.forEach((addOn) => {
+      estimatedPrice += addOnPrices[addOn][city] * carpetArea;
+  });
+
+  return estimatedPrice;
+  document.getElementById('result-project-type').textContent = projectType;
+  document.getElementById('result-city').textContent = city;
+  document.getElementById('result-property-type').textContent = propertyType;
+  document.getElementById('result-styling').textContent = styling;
+  document.getElementById('result-ceiling-height').textContent = ceilingHeight;
+  document.getElementById('result-plan-type').textContent = plan;
+  document.getElementById('result-carpet-area').textContent = carpetArea;
+  document.getElementById('result-add-ons').textContent = addOnValues.join(', ');
+  document.getElementById('result-estimated-price').textContent = estimatedPrice.toLocaleString(undefined, { maximumFractionDigits: 2 });
+ // Display price with two decimal places
 }
 
-// ...
 
-const addRoomBtn = document.getElementById("add-room-btn");
-addRoomBtn.addEventListener("click", addRoom);
 
-function addRoom() {
-  const roomIndex = roomsContainer.children.length;
+function generateAddOnsCheckboxes() {
+  const addOnsContainer = document.getElementById("add-ons-container");
 
-  const roomDiv = createRoomFields(roomIndex);
-  roomsContainer.appendChild(roomDiv);
+  for (const addOn in addOnPrices) {
+    const checkboxOption = document.createElement("div");
+    checkboxOption.className = "checkbox-option";
+
+    const addOnCheckbox = document.createElement("input");
+    addOnCheckbox.type = "checkbox";
+    addOnCheckbox.name = "add-on";
+    addOnCheckbox.value = addOn;
+
+    const addOnLabel = document.createElement("label");
+    addOnLabel.textContent = addOn;
+
+    checkboxOption.appendChild(addOnCheckbox);
+    checkboxOption.appendChild(addOnLabel);
+    addOnsContainer.appendChild(checkboxOption);
+  }
 }
 
-// ...
 
-const categoryOptions = document.getElementById("category-options");
-for (const category of workCategories) {
-  const categoryCheckbox = document.createElement("input");
-  categoryCheckbox.type = "checkbox";
-  categoryCheckbox.name = "selected-categories[]";
-  categoryCheckbox.value = category;
 
-  const categoryLabel = document.createElement("label");
-  categoryLabel.textContent = category;
-  categoryLabel.appendChild(categoryCheckbox);
+// Function to gather user selections
+function gatherUserSelections() {
+  const projectType = document.querySelector(".button-option-project.button-selected").dataset.value;
+  const city = document.querySelector(".button-option-city.button-selected").dataset.value;
+  const propertyType = document.querySelector(".button-option-property.button-selected").dataset.value;
+  const styling = document.querySelector(".button-option-styling.button-selected").dataset.value;
+  const ceilingHeight = document.querySelector(".button-option-ceiling.button-selected").dataset.value;
+  const plan = document.querySelector(".button-option-plan.button-selected").dataset.value;
+  const carpetArea = parseFloat(document.getElementById("carpet-area").value);
 
-  categoryOptions.appendChild(categoryLabel);
+  const selectedAddOns = [];
+  const addOnCheckboxes = document.querySelectorAll('input[name="add-on"]:checked');
+  addOnCheckboxes.forEach((checkbox) => {
+      selectedAddOns.push(checkbox.value);
+  });
+
+  return {
+      projectType,
+      city,
+      propertyType,
+      styling,
+      ceilingHeight,
+      plan,
+      carpetArea,
+      selectedAddOns,
+  };
 }
+
+
+// Function to update the result output
+function updateResultOutput(estimatedPrice, selections) {
+  const resultContainer = document.getElementById("result");
+
+  resultContainer.innerHTML = `
+      <h2>Estimation Results</h2>
+      <p><strong>Project Type:</strong> ${selections.projectType}</p>
+      <p><strong>City:</strong> ${selections.city}</p>
+      <p><strong>Property Type:</strong> ${selections.propertyType}</p>
+      <p><strong>Styling:</strong> ${selections.styling}</p>
+      <p><strong>Ceiling Height:</strong> ${selections.ceilingHeight}</p>
+      <p><strong>Plan Type:</strong> ${selections.plan}</p>
+      <p><strong>Carpet Area:</strong> ${selections.carpetArea} sq. ft.</p>
+      <p><strong>Add-ons:</strong> ${selections.selectedAddOns.join(", ")}</p>
+      <h3>Estimated Price:</h3>
+      <p><strong>₹${estimatedPrice.toFixed(2)}</strong></p>
+  `;
+}
+
+// Update estimated price on user interaction
+document.getElementById("calculate-btn").addEventListener("click", function () {
+  const selections = gatherUserSelections();
+  const estimatedPrice = calculateEstimate(selections);
+  updateResultOutput(estimatedPrice, selections);
+  
+});
+
+document.getElementById("Confirm-btn").addEventListener("click", function () {
+  const selections = gatherUserSelections();
+  const estimatedPrice = calculateEstimate(selections);
+  const resultUrl = `result.html?projectType=${selections.projectType}&city=${selections.city}&propertyType=${selections.propertyType}&styling=${selections.styling}&ceilingHeight=${selections.ceilingHeight}&plan=${selections.plan}&carpetArea=${selections.carpetArea}&selectedAddOns=${selections.selectedAddOns.join(",")}&estimatedPrice=${estimatedPrice}`;
+
+  // Redirect to the result.html page
+  window.open(resultUrl, "_blank");
+
+});
+
+// Generate add-ons checkboxes
+generateAddOnsCheckboxes();
+// ... (previous code)
+
+// Function to update the result output
+function updateResultOutput(estimatedPrice, selections) {
+    document.getElementById("result-project-type").textContent = selections.projectType;
+    document.getElementById("result-city").textContent = selections.city;
+    document.getElementById("result-property-type").textContent = selections.propertyType;
+    document.getElementById("result-styling").textContent = selections.styling;
+    document.getElementById("result-ceiling-height").textContent = selections.ceilingHeight;
+    document.getElementById("result-plan-type").textContent = selections.plan;
+    document.getElementById("result-carpet-area").textContent = selections.carpetArea;
+    document.getElementById("result-add-ons").textContent = selections.selectedAddOns.join(", ");
+    document.getElementById("result-estimated-price").textContent = estimatedPrice;
+}
+
+// ... (rest of the code)
+
+// Add event listeners for property type buttons
+const propertyTypeButtons = document.querySelectorAll(".button-option-property");
+propertyTypeButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    // Remove the .button-selected class from all buttons
+    propertyTypeButtons.forEach((btn) => btn.classList.remove("button-selected"));
+
+    // Add the .button-selected class to the clicked button
+    button.classList.add("button-selected");
+  });
+});
+
+// Add event listeners for city buttons
+const cityButtons = document.querySelectorAll(".button-option-city");
+cityButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    // Remove the .button-selected class from all buttons
+    cityButtons.forEach((btn) => btn.classList.remove("button-selected"));
+
+    // Add the .button-selected class to the clicked button
+    button.classList.add("button-selected");
+  });
+});
+
+// Add event listeners for ceiling height buttons
+const ceilingHeightButtons = document.querySelectorAll(".button-option-ceiling");
+ceilingHeightButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    // Remove the .button-selected class from all buttons
+    ceilingHeightButtons.forEach((btn) => btn.classList.remove("button-selected"));
+
+    // Add the .button-selected class to the clicked button
+    button.classList.add("button-selected");
+  });
+});
+
+// Add event listeners for styling buttons
+const stylingButtons = document.querySelectorAll(".button-option-styling");
+stylingButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    // Remove the .button-selected class from all buttons
+    stylingButtons.forEach((btn) => btn.classList.remove("button-selected"));
+
+    // Add the .button-selected class to the clicked button
+    button.classList.add("button-selected");
+  });
+});
+
+// Add event listeners for project type buttons
+const projectTypeButtons = document.querySelectorAll(".button-option-project");
+projectTypeButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    // Remove the .button-selected class from all buttons
+    projectTypeButtons.forEach((btn) => btn.classList.remove("button-selected"));
+
+    // Add the .button-selected class to the clicked button
+    button.classList.add("button-selected");
+  });
+});
+
+// Add event listeners for plan buttons
+const planButtons = document.querySelectorAll(".button-option-plan");
+planButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    // Remove the .button-selected class from all buttons
+    planButtons.forEach((btn) => btn.classList.remove("button-selected"));
+
+    // Add the .button-selected class to the clicked button
+    button.classList.add("button-selected");
+  });
+});
+
+// Add event listener for the "Calculate Estimate" button
+document.getElementById("calculate-btn").addEventListener("click", function () {
+  console.log("Button clicked!"); // Check if the button click event is registered
+  const selections = gatherUserSelections();
+  console.log("Selections:", selections); // Check if selections are correctly gathered
+  const estimatedPrice = calculateEstimate(selections);
+  console.log("Estimated Price:", estimatedPrice); // Check if estimated price is calculated correctly
+  updateResultOutput(estimatedPrice, selections);
+});
